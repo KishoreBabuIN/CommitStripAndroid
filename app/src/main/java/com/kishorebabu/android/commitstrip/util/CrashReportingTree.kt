ba5b60873c5e2f5758226844709ad46f252cbe2d
Kishore Babu <barykaed@gmail.com>
2017-10-05 16:34:22 +0530

    Fix: tag is Nullable in CrashReportingTree

--- app/src/main/java/com/kishorebabu/android/commitstrip/util/CrashReportingTree.kt
+++ app/src/main/java/com/kishorebabu/android/commitstrip/util/CrashReportingTree.kt
@@ -10,7 +10,7 @@ import timber.log.Timber
  */
 class CrashReportingTree : Timber.Tree() {
 
-    override fun log(priority: Int, tag: String, message: String, t: Throwable?) {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
             return
         }
